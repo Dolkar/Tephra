ce1abc3a8d3f5723ffbb76c53d879a36d0831559
Dolkar <1105712+Dolkar@users.noreply.github.com>
2024-02-05 20:44:17 +0000

    Setup for more than one example

--- examples/tephra_examples.cpp
+++ examples/tephra_examples.cpp
@@ -3,7 +3,14 @@
 
 #include <fstream>
 
-std::unique_ptr<Example> createExample(std::ofstream& logFile) {
+#if _WIN32
+    #include <shellapi.h>
+    #include <cwchar>
+#elif __linux__
+    #include <cstdlib>
+#endif
+
+std::unique_ptr<Example> createExample(std::ofstream& logFile, int exampleNumber) {
     if (!logFile.is_open()) {
         logFile.open("examples_log.txt");
     }
@@ -14,8 +21,17 @@ std::unique_ptr<Example> createExample(std::ofstream& logFile) {
 
 #if _WIN32
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
+    LPWSTR* argv;
+    int argc;
+    argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+    int exampleNumber = 0;
+    if (argc > 1) {
+        exampleNumber = std::abs(std::wcstol(argv[1], nullptr, 10));
+    }
+
     std::ofstream logFile;
-    std::unique_ptr<Example> example = createExample(logFile);
+    std::unique_ptr<Example> example = createExample(logFile, exampleNumber);
 
     WindowManager windowMgr;
     windowMgr.init(hInstance);
@@ -26,8 +42,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
 }
 #elif __linux__
 int main(int argc, char** argv) {
+    int exampleNumber = 0;
+    if (argc > 1) {
+        exampleNumber = std::abs(std::strtol(argv[1], nullptr, 10));
+    }
+
     std::ofstream logFile;
-    std::unique_ptr<Example> example = createExample(logFile);
+    std::unique_ptr<Example> example = createExample(logFile, exampleNumber);
 
     WindowManager windowMgr;
     windowMgr.init();
@@ -39,8 +60,13 @@ int main(int argc, char** argv) {
 #elif defined(VK_USE_PLATFORM_METAL_EXT)
 // Global function invoked from NS or UI views and controllers to create demo
 static void demo_main(struct Demo& demo, void* caMetalLayer, int argc, const char* argv[]) {
+    int exampleNumber = 0;
+    if (argc > 1) {
+        exampleNumber = std::abs(std::strtol(argv[1], nullptr, 10));
+    }
+
     std::ofstream logFile;
-    std::unique_ptr<Example> example = createExample(logFile);
+    std::unique_ptr<Example> example = createExample(logFile, exampleNumber);
 
     WindowManager windowMgr;
     windowMgr.init(caMetalLayer);
